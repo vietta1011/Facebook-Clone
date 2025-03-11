636d1851293715685b79c237389ad5fa42be44c4
vietta1011 <taquocviet1011@gmail.com>
2025-03-11 17:33:07 +0700

    commit folder common

--- Recommendation_System/src/common/commonFunction.js
+++ Recommendation_System/src/common/commonFunction.js
@@ -1,28 +1,33 @@
-import moment from "moment"
+import moment from "moment";
 
+// đây là một hàm để cập nhật thời gian là bao nhiêu lâu trước khi xảy ra các thông báo hay gì đó (dạng như đã kết bạn bao lâu hay các thông báo từ bao giờ)
 class CommonFunction {
-    getDayOfTimeWithDay(fromDate, toDate){
-        let m1 = moment(fromDate);
-        let m2 = moment(toDate);
-        return m2.diff(m1, "days");
-    }
+  getDayOfTimeWithDay(fromDate, toDate) {
+    let m1 = moment(fromDate);
+    let m2 = moment(toDate);
+    return m2.diff(m1, "days");
+  }
+  // check xem đã bao nhiêu ngày
 
-    getDayOfTimeWithHour(fromDate, toDate){
-        let m1 = moment(fromDate);
-        let m2 = moment(toDate);
-        return m2.diff(m1, "hours");
-    }
+  getDayOfTimeWithHour(fromDate, toDate) {
+    let m1 = moment(fromDate);
+    let m2 = moment(toDate);
+    return m2.diff(m1, "hours");
+  }
+  // check xem đã bao nhiêu giờ
 
-    getDayOfTimeWithMinutes(fromDate, toDate){
-        let m1 = moment(fromDate);
-        let m2 = moment(toDate);
-        return m2.diff(m1, "minutes");
-    }
+  getDayOfTimeWithMinutes(fromDate, toDate) {
+    let m1 = moment(fromDate);
+    let m2 = moment(toDate);
+    return m2.diff(m1, "minutes");
+  }
+  // check xem đã bao nhiêu phút
 
-    getDayOfTimeWithSecond(fromDate, toDate){
-        let m1 = moment(fromDate);
-        let m2 = moment(toDate);
-        return m2.diff(m1, "seconds");
-    }
+  getDayOfTimeWithSecond(fromDate, toDate) {
+    let m1 = moment(fromDate);
+    let m2 = moment(toDate);
+    return m2.diff(m1, "seconds");
+  }
+  // check xem đã bao nhiêu giây
 }
 export default new CommonFunction();
